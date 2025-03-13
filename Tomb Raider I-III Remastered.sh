4d2d597e89e40ef18d15f0363cf278ee9a9ea3ef
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-13 09:37:02 -0400

    Use new windows directory

--- Tomb Raider I-III Remastered.sh
+++ Tomb Raider I-III Remastered.sh
@@ -13,7 +13,7 @@ source $controlfolder/control.txt
 get_controls
 
 # Variables
-GAMEDIR=/$directory/ports/tombraider1-3
+GAMEDIR="/$directory/windows/tombraider1-3"
 
 # CD and set permissions
 cd $GAMEDIR/data
